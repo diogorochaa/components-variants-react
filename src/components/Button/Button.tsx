e84ad774623e34d2b2d26e634577949a49b34958
DiogoRocha10 <diogorocha12@live.com>
2022-05-27 19:35:51 -0300

    (feat/config) configuração do projeto storybook/create/ts

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,42 @@
+import styles from "./button.module.css";
+import cn from "clsx";
+
+interface ButtonProps {
+  variant?: string;
+  backgroundColor?: string;
+  size?: "sm" | "md" | "lg";
+  className?: string;
+  label: string;
+  onClick?: () => void;
+}
+
+export const Button = ({
+  variant,
+  size = "md",
+  backgroundColor,
+  label,
+  className,
+  ...props
+}: ButtonProps) => {
+  const sizeClasses = {
+    [styles["-button--small"]]: size === "sm",
+    [styles["-button--medium"]]: size === "md",
+    [styles["-button--large"]]: size === "lg",
+  };
+
+  const variantClasses = {
+    [styles["-button--primary"]]: variant === "primary",
+    [styles["-button--secondary"]]: variant === "secondary",
+  };
+
+  return (
+    <button
+      type="button"
+      className={cn(className, variantClasses, sizeClasses)}
+      style={{ backgroundColor }}
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
