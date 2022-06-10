0273f14a9c7f42ff0de84fd213eaa9ad2bb65d38
DiogoRocha10 <diogorocha12@live.com>
2022-06-10 19:24:03 -0300

    pipeline github actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import { Card } from "./components/Card"
 import styles from "./App.module.css"
-import tableRef from "./service/main"
-import { useEffect, useState } from "react"
-import { onValue } from "firebase/database"
+
+import { useState } from "react"
 
 function App() {
 	const [distance, setDistance] = useState(0)
-
-	useEffect(() => {
-		onValue(tableRef, (value) => {
-			setDistance(+value.val())
-		})
-	}, [])
 
 	return (
 		<div className={styles.App}>
